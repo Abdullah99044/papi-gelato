941c863a3ebb10f15fe55f4e4b036ee70849ad1b
Abdullah <ab-benkathir@outlook.com>
2021-10-26 23:16:30 +0200

    F1.5.02.O6 - Feature: Toppings

--- papiGelato.py
+++ papiGelato.py
@@ -4,8 +4,10 @@ def sorryFunction():
     print(".........................")
     print("Sorry dat snap ik niet...")
     print(".........................")
+
 def stap_3_Function(n , y ):
     print("Hier is uw " , y , " met " , n , "bolletje(s)" )
+
 def eindeFunction():
     print("Wilt u nog meer bestellen?")
     while True: 
@@ -36,22 +38,44 @@ def smakenFuction(x):
             else:
                 print("Sorry dat snap ik niet...")
 
-def bonenFunction(x , y , z):
+def bonenFunction(x , y , z , i , n , c):
     r1 = x * 1.10
     r2 = y * 1.25
     r3 = z * 0.75
-    total = r1 + r2 + r3
+    r4 = i * 1
+    total = r1 + r2 + r3 + i
+    if c == "C" and i > 0.3 :
+        n = x
     print("----------[Papi Gelato]----------")
-    print("Bolletjes  ", x , " x " " €1,10   =", r1)
-    print("Horrentjes ", y , " x " " €1,25   =", r2 )
-    print("Bakjes     ", z , " x " " €0,75   =", r3)
+    print("Bolletjes  ", x , " x " " € 1,10   =","€",r1)
+    print("Horrentjes ", y , " x " " € 1,25   =","€",r2 )
+    print("Bakjes     ", z , " x " " € 0,75   =","€",r3)
+    print("Topping    ", n , " x " ,"€",i , "   =","€",r4 )
     print("                          ----- +")
-    print("Total                    =", total)
+    print("Total                    =", "€", total)
 
-
+def toppingFunction(i , n , A):
+    if  A == "A" :
+        return 0
+    elif A == "B" :
+        return 0.5
+    elif A == "C" :
+        return 0.3 * n 
+    elif A == "D" and i == 2:
+        return 0.9
+    elif A == "D" and i == 1:
+        return 0.6
+    
+def toppingFunction1():
+    print("Wat voor topping wilt u: ")
+    print(" A) Geen ")
+    print(" B) Slagroom" )
+    print(" C) Sprinkels ( prijs x aantal bolletjes ")
+    print(" D) Caramel Saus ")
 
 def knopF():
     a = input("Druk op een knop om terug te gaan naar het begin")
+
 def programFunction():
     while True:
         print(".........................")
@@ -67,13 +91,17 @@ def programFunction():
                 if answer1 == "A":
                     stap_3_Function(answer , "hoorntje") 
                     if answer != 0: 
-                        bonenFunction(answer , 1 , 0)
+                        toppingFunction1()
+                        answer2 = input(" : ")
+                        bonenFunction(answer , 1 , 0 , toppingFunction(1 , answer , answer2), 1 , answer2)
                     eindeFunction()
                     break          
                 elif answer1 == "B":
                     stap_3_Function(answer , "bakje")
                     if answer != 0: 
-                        bonenFunction(answer , 1 , 0)
+                        toppingFunction1()
+                        answer2 = input(" : ")
+                        bonenFunction(answer , 0 , 1 , toppingFunction(2, answer , answer2) , 1  , answer2 )
                     eindeFunction()
                     break
                 else:
@@ -84,7 +112,9 @@ def programFunction():
             print("Dan krijgt u van mij een bakje met " , answer , "bolletjes")
             smakenFuction(answer)
             if answer != 0: 
-                bonenFunction(answer , 1 , 0)
+                toppingFunction1()
+                answer2 = input(" : ")
+                bonenFunction(answer , 0 , 1 , toppingFunction(2, answer , answer2) , 1  , answer2 )
             eindeFunction()
             break
         elif answer > 8:
@@ -92,7 +122,6 @@ def programFunction():
                 print("Sorry, zulke grote bakken hebben we niet")
                 print("........................................")
                 knopF()
-
         else :
             sorryFunction()
             knopF()
