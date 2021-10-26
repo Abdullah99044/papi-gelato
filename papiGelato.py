57e784a58b54268d63b42c45d6207c3d371c5368
Abdullah <ab-benkathir@outlook.com>
2021-10-26 11:19:33 +0200

    F1.5.02.O3 - 3 nieuwe smaken

--- papiGelato.py
+++ papiGelato.py
@@ -1,12 +1,12 @@
-
 #Functions
 
 def sorryFunction():
     print(".........................")
     print("Sorry dat snap ik niet...")
     print(".........................")
-def stap_3_Function(n , y):
+def stap_3_Function(n , y ):
     print("Hier is uw " , y , " met " , n , "bolletje(s)" )
+def eindeFunction():
     print("Wilt u nog meer bestellen?")
     while True: 
         answer = input("(Y/N) :")  
@@ -16,9 +16,27 @@ def stap_3_Function(n , y):
             programFunction()  
         elif answer == "N" :
             print("Bedankt en tot ziens!") 
-            break  
+            return  
         else :
-            sorryFunction()        
+            sorryFunction()      
+
+def smakenFuction(x):
+    z = 1
+    for i in range(x):
+        while True:
+            print("Welke smaak wilt u voor bolletje nummer?" , z)
+            print("A) Aardbei")
+            print("C) Chocolade")
+            print("M) Munt")
+            print("V) Vanille?")
+            k = input("A , C , M , OF V ? : ")
+            if k == "A" or "C" or "M" or "V" :
+                z += 1
+                break
+            else:
+                print("Sorry dat snap ik niet...")
+
+
 def knopF():
     a = input("Druk op een knop om terug te gaan naar het begin")
 def programFunction():
@@ -27,6 +45,7 @@ def programFunction():
         answer = int(input("Hoeveel bolletjes wilt u? : "))
         print(".........................")
         if answer <= 4 :
+            smakenFuction(answer)
             print("Wilt u deze " , answer , " bolletje(s) in")
             print("A) een hoorntje? ")
             print("B) of een bakje?")
@@ -34,9 +53,11 @@ def programFunction():
                 answer1 = input("A/B")
                 if answer1 == "A":
                     stap_3_Function(answer , "hoorntje")  
+                    eindeFunction()
                     break          
                 elif answer1 == "B":
                     stap_3_Function(answer , "bakje")
+                    eindeFunction()
                     break
                 else:
                     sorryFunction()
@@ -44,8 +65,8 @@ def programFunction():
         elif answer > 4 and answer <= 8:
             print("..........................................................")
             print("Dan krijgt u van mij een bakje met " , answer , "bolletjes")
-            print("..........................................................")
-            stap_3_Function(answer , "bolletjes" )
+            smakenFuction(answer)
+            eindeFunction()
             break
         elif answer > 8:
                 print("........................................")
